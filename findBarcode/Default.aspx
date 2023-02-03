87c1716f2d3f2f0d266fb403001d0f7d7d764b3e
Ali Yenigün <73249432+aliyenigunn@users.noreply.github.com>
2023-02-03 18:13:02 +0200

    Add project files.

---
+++ findBarcode/Default.aspx
@@ -0,0 +1,80 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="findBarcode.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Find Barcode</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <link href="Content/bootstrap.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="card p-2">
+            <asp:Label ID="Label1" runat="server" Text="Label" Visible="false"></asp:Label>
+            <asp:Image ID="ImageBarcode" Width="400px" Height="150px" runat="server" Visible="false" />
+        </div>
+        <div>
+            <asp:TextBox ID="txtFind" runat="server"></asp:TextBox>
+            <asp:Button ID="btnSearch" runat="server" class="btn btn-outline-secondary" OnClick="btnSearch_Click" Text="ARA" />
+            <asp:Button ID="btnClear" runat="server" class="btn btn-outline-secondary" OnClick="btnClear_Click" Text="SİL" />
+
+        </div>
+        <div class="card">
+            <div class="card-header">
+                <h3 id="txtSonuc" runat="server"></h3>
+            </div>
+            <div class="card-body">
+                <table class="table table-responsive-lg table-hover table-stripped table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Amcası</th>
+                            <th>Barkodu</th>
+                            <th>Adı</th>
+                            <th>Kodu</th>
+                            <th>Birimi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <asp:PlaceHolder ID="mainHolder" runat="server"></asp:PlaceHolder>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <%--<div>
+            <asp:GridView ID="GridView1" runat="server"
+                CssClass="table table-responsive-lg table-stripped  table-hover"
+                AutoGenerateColumns="False" DataSourceID="SqlDataSource1"
+                OnRowCommand="GridView1_RowCommand" AllowSorting="True"
+                BorderStyle="Solid" BorderWidth="3px" CellPadding="4" CellSpacing="2">
+                <Columns>
+                    <asp:ButtonField ButtonType="Button" Text="GÖSTER" />
+                    <asp:BoundField DataField="BARCODE" HeaderText="BARKOD" SortExpression="BARCODE" />
+                    <asp:BoundField DataField="NAME" HeaderText="ÜRÜN ADI" SortExpression="NAME" />
+                    <asp:BoundField DataField="CODE" HeaderText="ÜRÜN KODU" SortExpression="CODE" />
+                    <asp:BoundField DataField="BIRIM" HeaderText="BİRİM" SortExpression="BIRIM" />
+                </Columns>
+                <FooterStyle BackColor="#CCCCCC" />
+                <HeaderStyle BackColor="Black" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#CCCCCC" ForeColor="Black" HorizontalAlign="Left" />
+                <RowStyle BackColor="White" />
+                <SelectedRowStyle BackColor="#000099" Font-Bold="True" ForeColor="White" />
+                <SortedAscendingCellStyle BackColor="#F1F1F1" />
+                <SortedAscendingHeaderStyle BackColor="Gray" />
+                <SortedDescendingCellStyle BackColor="#CAC9C9" />
+                <SortedDescendingHeaderStyle BackColor="#383838" />
+            </asp:GridView>
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:LKSDBConnectionString %>" SelectCommand="SELECT  [BARCODE], [NAME], [CODE], [BIRIM] FROM [PAZ_DEPOBAZLISTOKFIYAT]" FilterExpression="[NAME] LIKE '%{0}%'">
+                <FilterParameters>
+                    <asp:ControlParameter Name="NAME"
+                        ControlID="txtFind" PropertyName="Text" />
+                </FilterParameters>
+            </asp:SqlDataSource>
+        </div>--%>
+    </form>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ findBarcode/Default.aspx.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Data;
+using System.Drawing.Imaging;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Text;
+using System.Linq;
+using Bytescout.BarCode;
+using System.Text.RegularExpressions;
+
+namespace findBarcode
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private string SearchString = "";
+        StringBuilder sb = new StringBuilder();
+        barkodDbDataContext con = new barkodDbDataContext();
+        void getMetot(string sBuilder)
+        {
+            try
+            {
+                mainHolder.Controls.Add(new Literal { Text = sBuilder });
+
+            }
+            catch (Exception ex)
+            {
+                ScriptManager.RegisterStartupScript(this, GetType(), "ServerControlScript", ex.Message, true);
+            }
+        }
+        string getTable(string code)
+        {
+
+            try
+            {
+                var q = con.PAZ_DEPOBAZLISTOKFIYATs.ToList();
+                if (code != "")
+                {
+
+                    q = q.Where(x => x.NAME.Contains(code)).ToList();
+              
+                }
+                foreach (var item in q)
+                {
+                    sb.Append("<tr>");
+                    sb.Append("<td><a href='Default.aspx?productCode=" + item.BARCODE + "&productName="+item.NAME+"' class='btn btn-primary'>Göster</a> </td>");
+                    sb.Append("<td>" + item.BARCODE + "</td>");
+                    sb.Append("<td>" + item.NAME + "</td>");
+                    sb.Append("<td>" + item.CODE + "</td>");
+                    sb.Append("<td>" + item.BIRIM + "</td>");
+                    sb.Append("</tr>");
+                }
+                getMetot(sb.ToString());
+                return sb.ToString();
+            }
+            catch (Exception ex)
+            {
+
+                return ex.Message;
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                getTable("");
+                if (Request.QueryString["productCode"] != null)
+                {
+                    GenerateBarcode(Request.QueryString["productCode"].ToString());
+                }
+            }
+
+            if (txtFind.Text.Length > 1)
+            {
+                getTable(txtFind.Text.ToUpper());
+            }
+
+            if (Page.IsPostBack)
+            {
+                getTable(txtFind.Text);
+                if (Request.QueryString["productCode"] != null)
+                {
+                    GenerateBarcode(Request.QueryString["productCode"].ToString());
+                    //if (Session["_errCode"] != null)
+                    //    ScriptManager.RegisterStartupScript(this, GetType(), "ServerControlScript", Session["_errCode"].ToString(), true);
+                }
+            }
+        }
+        //protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        //{
+        //    try
+        //    {
+        //        int rowIndex = Convert.ToInt32(e.CommandArgument);
+        //        //string barcode = GridView1.Rows[rowIndex].Cells[1].Text;
+        //        //string lbl = GridView1.Rows[rowIndex].Cells[2].Text;
+        //        //MessageBox.Show(barcode , "BARCODE",MessageBoxButton.OK);
+        //        GenerateBarcode(barcode, lbl);
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Response.Write(ex.Message);
+        //    }
+        //}
+        private void GenerateBarcode(string code)
+        {
+            try
+            {
+                Barcode barcode = new Barcode();
+
+                // Set symbology
+                barcode.Symbology = SymbologyType.EAN13;
+                // Set value
+                barcode.Value = code;
+
+                // Clear http output
+                Response.Clear();
+                // Set the content type to PNG
+                Response.ContentType = "image/png";
+                // Add content type header
+                Response.AddHeader("Content-Type", "image/png");
+                // Set the content disposition
+                Response.AddHeader("Content-Disposition", "inline;filename=result.png");
+
+                // Save image to output stream
+                barcode.SaveImage(Response.OutputStream, ImageFormat.Png);
+
+                // End response
+                Response.End();
+                Label1.Visible = true;
+                Label1.Text = Request.QueryString["productName"].ToString();
+            }
+            catch (Exception ex)
+            {
+                ScriptManager.RegisterStartupScript(this, GetType(), "ServerControlScript", ex.Message, true);
+                Session.Add("_errCode", ex.Message);
+            }
+            
+        }
+        //public string HighlightText(string InputTxt)
+        //{
+        //    string Search_Str = txtFind.Text;
+        //    Regex RegExp = new Regex(Search_Str.Replace(" ", "|").Trim(), RegexOptions.IgnoreCase);
+        //    return RegExp.Replace(InputTxt, new MatchEvaluator(ReplaceKeyWords));
+        //}
+        //public string ReplaceKeyWords(Match m)
+        //{
+        //    return ("<span class=highlight>" + m.Value + "</span>");
+        //}
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            SearchString = txtFind.Text;
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            txtFind.Text = "";
+            SearchString = "";
+            //GridView1.DataBind();
+        }
+    }
+}
